277e585f6675765d6d6e1e9ae96cf390d4289165
darkstar331 <harman6549@gmail.com>
2024-08-30 04:25:52 -0600

    build failed resolved

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -1,29 +1,29 @@
-'use client'
 import { useState, useEffect } from 'react';
-import QuietAreaForm from '../components/QuietAreaForm';
-import Map from '../components/Map';
+import QuietAreaForm from '../../components/QuietAreaForm';
+import Map from '../../components/Map';
 import axios from 'axios';
 
-// getServerSideProps function to fetch data on the server side before rendering the page
-export async function getServerSideProps() {
-  try {
-    const response = await axios.get('https://usafe-uoa.netlify.app/api/map');
-    if (response.status === 200) {
-      return { props: { initialTaggedLocations: response.data.data } };
-    } else {
-      return { props: { initialTaggedLocations: [] } };
-    }
-  } catch (error) {
-    console.error('Error fetching tagged locations:', error);
-    return { props: { initialTaggedLocations: [] } };
-  }
-}
-
-export default function MapPage({ initialTaggedLocations }) {
-  const [taggedLocations, setTaggedLocations] = useState(initialTaggedLocations);
+export default function MapPage() {
+  const [taggedLocations, setTaggedLocations] = useState([]);
   const [userLocation, setUserLocation] = useState(null);
 
   useEffect(() => {
+    // Fetch all tagged locations when the component mounts
+    const fetchTags = async () => {
+      try {
+        const response = await axios.get('https://usafe-uoa.netlify.app/api/map');
+        if (response.status === 200) {
+          setTaggedLocations(response.data.data);
+        } else {
+          console.error('Failed to fetch tagged locations');
+        }
+      } catch (error) {
+        console.error('Error fetching tagged locations:', error);
+      }
+    };
+
+    fetchTags();
+
     // Optionally fetch the user's current location
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
